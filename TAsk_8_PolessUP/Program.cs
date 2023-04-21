3a94ce308d6f483ac49d5ea06e496d1e5ec7ceb5
Ylixandr <savoshinsky@inbox.ru>
2023-04-21 13:55:42 +0300

    Добавьте файлы проекта.

---
+++ TAsk_8_PolessUP/Program.cs
@@ -0,0 +1,15 @@
+﻿static string RgbToHex(int r, int g, int b)
+{
+    // Округляем значения до ближайшего допустимого значения
+    r = Math.Max(0, Math.Min(255, r));
+    g = Math.Max(0, Math.Min(255, g));
+    b = Math.Max(0, Math.Min(255, b));
+
+    // Конвертируем значения RGB в шестнадцатеричную систему счисления
+    string hex = r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+
+    return hex;
+}
+
+string hex = RgbToHex(255, 255, 255);
+Console.WriteLine(hex);
